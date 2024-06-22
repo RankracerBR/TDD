2c4e17939effb3c8286aadc744b94773bac317bc
Augusto Pontes <augustopontes010@gmail.com>
2024-06-22 15:43:46 -0300

    TDD

--- baconcomovos.py
+++ baconcomovos.py
@@ -9,3 +9,16 @@
 5 - Saber se o número NÃO é múltiplo de 3 e 5:
     Passa fome
 """
+def bacon_com_ovos(n):
+    assert isinstance(n, int), 'n deve ser int'
+
+    if n % 3 == 0 and n % 5 == 0:
+        return 'Bacon com ovos'
+    
+    if n % 3 == 0:
+        return 'Bacon'
+    
+    if n % 5 == 0:
+        return 'Ovos'
+
+    return 'Passar fome'
